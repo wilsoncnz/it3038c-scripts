91d6ecd3482b129881a0f1ae93f55f579501e51c
Chloe Wilson Lorenzen <wilsoncz@mail.uc.edu>
2021-11-07 22:00:51 -0500

    Lab 9

--- labs/Lab9.py
+++ labs/Lab9.py
@@ -5,7 +5,10 @@ import json
 r = requests.get('http://localhost:3000')
 data = r.json()
 
-for x in range(len(data)):
-    print(data[x]['name'].capitalize() + ' is ' +data[x]['color'] + ".")
+
+print(data[0]['name'].capitalize() + ' is ' +data[0]['color'] + ".")
+print(data[1]['name'].capitalize() + ' is ' +data[1]['color'] + ".")
+print(data[2]['name'].capitalize() + ' is ' +data[2]['color'] + ".")
+print(data[3]['name'].capitalize() + ' is ' +data[3]['color'] + ".")
 
 
